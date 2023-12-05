8905fcbe38b4215b1d62a93827a12fc0910ecf12
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2023-12-05 13:48:57 -0800

    display a schedule for only a specified day.
    Co-authored-by: Monica Barboza <MonBoza@users.noreply.github.com>
    Co-authored-by: Kim Robinson <kimmykokonut@users.noreply.github.com>

--- src/components/MarketControl.jsx
+++ src/components/MarketControl.jsx
@@ -1,6 +1,46 @@
 import React from 'react';
-import MarketSchedule from './MarketSchedule';
-import Button from './Button';
+//import MarketSchedule from './MarketSchedule';
+// import Button from './Button';
+import Day from './Day';
+
+const marketSchedule = [
+    {
+        day: "Sunday",
+        location: "Lents International",
+        hours: "9:00am - 2:00pm",
+        booth: "4A"
+    },
+    {
+        day: "Monday",
+        location: "Pioneer Courthouse Square",
+        hours: "10:00am - 2:00pm",
+        booth: "7C"
+    },
+    {
+        day: "Tuesday",
+        location: "Hillsboro",
+        hours: "5:00pm - 8:30pm",
+        booth: "1F"
+    },
+    {
+        day: "Wednesday",
+        location: "Shemanski Park",
+        hours: "10:00am - 2:00pm",
+        booth: "3E"
+    },
+    {
+        day: "Thursday",
+        location: "Northwest Portland",
+        hours: "2:00pm - 6:00pm",
+        booth: "6D"
+    },
+    {
+        day: "Saturday",
+        location: "Beaverton",
+        hours: "10:00am - 1:30pm",
+        booth: "9G"
+    }
+];
 
 class MarketControl extends React.Component {
 
@@ -20,16 +60,25 @@ class MarketControl extends React.Component {
     render() {
         let visibleDay = null;
 
-        if (this.state.dayCounter === 0) {
-            visibleDay = <MarketSchedule />
+        if (this.state.dayCounter < 6) {
+            visibleDay = marketSchedule[this.state.dayCounter];
+            console.log(this.state.dayCounter);
+        } else {
+            console.log("you re in else");
+            this.setState({ dayCounter: 0 });
+            console.log(this.state.dayCounter);
+            visibleDay = marketSchedule[this.state.dayCounter];
         }
         return (
             <>
-                {visibleDay}
-                {/* <Button
-                    id="btnDay"
-                    text={visibleDay.day}
-                /> */}
+                <h3>Market Info</h3>
+                <Day
+                    day={visibleDay.day}
+                    location={visibleDay.location}
+                    hours={visibleDay.hours}
+                    booth={visibleDay.booth}
+                    key={this.state.dayCounter} />
+
                 <button onClick={this.handleClick}>Change Day</button>
             </>
         )
